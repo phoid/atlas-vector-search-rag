fe170fad8664560a212c60f4be9af3ae257e65ab
Patrick Reynolds <patrey15@gmail.com>
2024-03-13 12:38:00 -0700

    created Git Ignore
    Protecting API key, obviously

--- keys.py
+++ keys.py
@@ -1,2 +1,2 @@
 MONGO_URI = "mongodb+srv://patrick:Lampie12@clusterdev.uf4uqrq.mongodb.net/"
-OPENAI_KEY = "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"
+OPENAI_KEY = "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"
