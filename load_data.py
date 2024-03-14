b5a40026f4f8202a6079e303b15f97b5b38e2f30
Patrick Reynolds <patrey15@gmail.com>
2024-03-14 11:26:55 -0700

    updated Load_data
    updated Langchain OpenAI embedding and mongoDBAtlas

--- load_data.py
+++ load_data.py
@@ -1,38 +1,71 @@
 from pymongo import MongoClient
-from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores.mongodb_atlas import MongoDBAtlasVectorSearch
-from langchain_community.document_loaders.directory import DirectoryLoader
 from langchain_community.llms import OpenAI
 import keys
+import getpass
+import os
+
+# os.environ[keys.OPENAI_KEY] = getpass.getpass()
+
+directory = os.fsencode("samples")
 
 # Set the MongoDB URI, DB, Collection Names
 
 client = MongoClient(keys.MONGO_URI)
-dbName = "langchain_demo"
-collectionName = "texts"
-collection = client[dbName][collectionName]
+database = client["langchain_demo"]
+collection = database["Text"]
 
-# Initialize the DirectoryLoader
-loader = DirectoryLoader("./samples", glob="./*.txt", show_progress=True)
-data = loader.load()
+embeddings = OpenAIEmbeddings(model="text-embedding-3-large", api_key=keys.OPENAI_KEY)
 
-# Define the OpenAI Embedding Model we want to use for the source data
-# The embedding model is different from the language generation model
+
+def process_text_file():
+    for file in os.scandir(directory):
+
+        file = open(file)
+        text_content = file.read()
+        embedded = embeddings.embed_query(text_content)
+        document = {
+            "title": os.fsdecode(file.name)[8:],
+            "text_content": text_content,
+            "embedded_content": embedded,
+        }
+        collection.insert_one(document)
+
+
 embeddings = OpenAIEmbeddings(openai_api_key=keys.OPENAI_KEY)
 
-# Initialize the VectorStore, and
-# vectorise the text from the documents using the specified embedding model, and insert them into the specified MongoDB collection
-vectorStore = MongoDBAtlasVectorSearch.from_documents(
-    data, embeddings, collection=collection
-)
-atlasSI = {
-    "fields": [
+process_text_file()
+
+
+def text_to_embedding(text):
+    return embeddings
+
+
+# Store embeddings in MongoDB alongside your documents
+for doc in collection.find():
+    embedding = text_to_embedding(doc["text_content"])
+    collection.update_one(
+        {"_id": doc["_id"]}, {"$set": {"embedding": embedding.tolist()}}
+    )
+
+
+# Vector search
+def search_by_vector(query_text, top_n=3):
+    query_embedding = text_to_embedding(query_text)
+
+    results = collection.find(
         {
-            "path": "embedding",
-            "numDimensions": 1536,
-            "similarity": "cosine",
-            "type": "vector",
+            "embedding": {
+                "$near": {
+                    "$geometry": {
+                        "type": "Point",
+                        "coordinates": query_embedding.tolist(),
+                    },
+                    "$maxDistance": 1000,  # Adjust threshold as needed
+                }
+            }
         }
-    ]
-}
-client.get_database(dbName).get_collection(collectionName).create_index(atlasSI)
+    )
+
+    return list(results)[:top_n]
